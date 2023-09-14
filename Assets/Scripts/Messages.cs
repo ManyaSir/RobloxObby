80c79d86eaaa82f2b332b0f15f303cc0d36a1dfa
ManyaSir <y10koehuma@gmail.com>
2023-09-14 21:20:02 +0900

    чуток подправил

---
+++ Assets/Scripts/Messages.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Messages : MonoBehaviour
+{
+    
+    //GameManager.value = 2;
+    
+    void Start()
+    {
+        //value++;
+    }
+
+}
