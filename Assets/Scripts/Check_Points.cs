1ec7a3f14c25a0d7976814ac5738fc2dcae7999f
ManyaSir <y10koehuma@gmail.com>
2023-09-17 03:13:41 +0900

    ЗАКОНЧИЛ СКРИПТЫ НА ЧЕКПОИНТЫ

--- Assets/Scripts/Check_Points.cs
+++ Assets/Scripts/Check_Points.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Globalization;
 
 public class Check_Points : MonoBehaviour
 {
-    [SerializeField] Transform Latest_Checkpoint;
+    [SerializeField] public Transform Latest_Checkpoint;
     [SerializeField] Transform Latest_BlockWall;
-    [SerializeField] private GameObject Player;
+    [SerializeField] public GameObject Player;
     [SerializeField] public Transform Spawn;
     [SerializeField] public Transform ZeroCount;
     [SerializeField] public Transform Latest_Checkpoint_count;
@@ -25,14 +26,47 @@ public class Check_Points : MonoBehaviour
         
     }
     
-    void Start()
+    Vector3 StringToVector3(string input)
     {
-        if(this.CompareTag("Checkpoint"))
-        {
-            Latest_Checkpoint.transform.position = Latest_Checkpoint_count.transform.position;
-            Player.transform.position = Latest_Checkpoint.transform.position;
-        }
+        string[] values = input.Split(',', '(', ')');
+        float x = float.Parse(values[1].Trim(), CultureInfo.InvariantCulture.NumberFormat);
+        float y = float.Parse(values[2].Trim(), CultureInfo.InvariantCulture.NumberFormat);
+        float z = float.Parse(values[3].Trim(), CultureInfo.InvariantCulture.NumberFormat);
+        return new Vector3(x, y, z);
+    }
+    
+    void Start()  
+    {  
+        
+        if(this.CompareTag("Checkpoint"))  
+        {  
+            
+            GameManager.Latest_Checkpoint_pos = PlayerPrefs.GetString("StringLatest_Checkpoint");  
 
+            
+            GameManager.Latest_Checkpoint_ToVector3 = StringToVector3(GameManager.Latest_Checkpoint_pos);  
+                
+            
+            Latest_Checkpoint_count.transform.position = GameManager.Latest_Checkpoint_ToVector3;  
+                
+            
+            Latest_Checkpoint.transform.position = Latest_Checkpoint_count.transform.position;  
+                
+            
+            Player.transform.position = Latest_Checkpoint.transform.position;
+
+
+            GameManager.Latest_BlockWall_pos = PlayerPrefs.GetString("StringLatest_BlockWall");
+
+
+            GameManager.Latest_BlockWall_ToVector3 = StringToVector3(GameManager.Latest_BlockWall_pos);
+
+            
+            Latest_BlockWall_count.transform.position = GameManager.Latest_BlockWall_ToVector3;
+            
+
+            Latest_BlockWall.transform.position = Latest_BlockWall_count.transform.position;
+        } 
     }
 
     void OnTriggerEnter(Collider other) 
@@ -43,14 +77,46 @@ public class Check_Points : MonoBehaviour
             Destroy(this.gameObject); 
             Latest_BlockWall_count.transform.position = Latest_BlockWall_place.transform.position;
             Latest_BlockWall.transform.position = Latest_BlockWall_count.transform.position;
+            GameManager.Latest_BlockWall_pos = Latest_BlockWall_count.transform.position.ToString();
+            PlayerPrefs.SetString("StringLatest_BlockWall", GameManager.Latest_BlockWall_pos);
+            PlayerPrefs.Save();
         } 
         if(this.CompareTag("Checkpoint") && other.CompareTag("Player"))
         {
             Latest_Checkpoint_count.transform.position = this.transform.position;
             Latest_Checkpoint.transform.position = Latest_Checkpoint_count.transform.position;
+            GameManager.Latest_Checkpoint_pos = Latest_Checkpoint_count.transform.position.ToString();
+            PlayerPrefs.SetString("StringLatest_Checkpoint", GameManager.Latest_Checkpoint_pos);
+            PlayerPrefs.Save();
 
         }
+        if(this.CompareTag("Death_Ground") && other.CompareTag("Player"))
+        {
+            GameManager.Latest_Checkpoint_pos = PlayerPrefs.GetString("StringLatest_Checkpoint");  
+            
+            GameManager.Latest_Checkpoint_ToVector3 = StringToVector3(GameManager.Latest_Checkpoint_pos);  
+                
+            
+            Latest_Checkpoint_count.transform.position = GameManager.Latest_Checkpoint_ToVector3;  
+                
+            
+            Latest_Checkpoint.transform.position = Latest_Checkpoint_count.transform.position;  
+                
+            
+            Player.transform.position = Latest_Checkpoint.transform.position;  
+        }
     }
+
+    public void Reset()
+    {
+        PlayerPrefs.SetString("StringLatest_Checkpoint", null);
+        PlayerPrefs.SetString("StringLatest_BlockWall", null);
+        PlayerPrefs.SetInt("Money", 0   );
+        PlayerPrefs.SetInt("Current_Lvl", 0);
+        PlayerPrefs.Save();
+        SceneManager.LoadScene("Game");
+    }
+
 
     
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,7 +12,11 @@ public class GameManager : MonoBehaviour
     public int Money = 0;
     public int Current_Lvl = 0;
     [SerializeField] private Text lvls_count;
-    [SerializeField] private Text money_count;   
+    [SerializeField] private Text money_count;  
+    public static string Latest_Checkpoint_pos; 
+    public static string Latest_BlockWall_pos;
+    public static Vector3 Latest_BlockWall_ToVector3;
+    public static Vector3 Latest_Checkpoint_ToVector3;
 
     
     void Start()
