f309baa3814eb92a901ed6d25eb150615eb357a4
ManyaSir <y10koehuma@gmail.com>
2023-09-17 22:53:42 +0900

    Сделал улучшенную кастомизацию

---
+++ Assets/Scripts/Custom.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Custom : MonoBehaviour
+{
+    
+    public GameObject Accessory;
+    
+    public void Customing()
+    {
+        GameObject[] accessories = GameObject.FindGameObjectsWithTag("Accessory");
+        foreach(GameObject accessory in accessories) 
+        {
+            accessory.SetActive(false);
+        }
+        Accessory.SetActive(true);
+    }
+
+    public void Buy10()
+    {
+        if(PlayerPrefs.GetInt("Money") >= 10)
+        {
+            PlayerPrefs.SetInt("Money", PlayerPrefs.GetInt("Money") - 10);
+            PlayerPrefs.Save();
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void Buy25()
+    {
+        if(PlayerPrefs.GetInt("Money") >= 25)
+        {
+            PlayerPrefs.SetInt("Money", PlayerPrefs.GetInt("Money") - 25);
+            PlayerPrefs.Save();
+            Destroy(this.gameObject);
+
+        }
+    }
+
+    public void Buy50()
+    {
+        if(PlayerPrefs.GetInt("Money") >= 50)
+        {
+            PlayerPrefs.SetInt("Money", PlayerPrefs.GetInt("Money") - 50);
+            PlayerPrefs.Save();
+            Destroy(this.gameObject);
+
+        }
+    }
+    
+}
