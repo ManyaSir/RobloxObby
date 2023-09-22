48cbbdc664e7cd54d4bd29de9dcbcbf4c0c76e8d
ManyaSir <y10koehuma@gmail.com>
2023-09-22 20:30:52 +0900

    Сделал Меню

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SceneLoader : MonoBehaviour
+public class Buttons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject Menu;
+    [SerializeField] private GameObject Settings;
+    [SerializeField] private GameObject Skins;
+    [SerializeField] private GameObject Pause;
+    [SerializeField] public GameObject Player;
+
+
+    
+    public void NewGame()
     {
-        
+        //UpInf()
+        Menu.SetActive(false);
+        Player.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Continue()
     {
-        
+        //Menu.SetActive
     }
 }
